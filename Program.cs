a5427d79daa5b6228011d79312d6082de20070c4
ZernZ <104818587+ZernZz@users.noreply.github.com>
2023-05-19 23:21:03 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class MainClass {
+    public static void Main (string[] args) {
+        string[] inputs = Console.ReadLine().Split(' ');
+        int N = int.Parse(inputs[0]);
+        int K = int.Parse(inputs[1]);
+        int[] population = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int maxCustomers = 0;
+        for (int i = 0; i < N; i++) {
+            int totalCustomers = 0;
+            for (int j = Math.Max(0, i - K); j <= Math.Min(i + K, N - 1); j++) {
+                totalCustomers += population[j];
+            }
+            maxCustomers = Math.Max(maxCustomers, totalCustomers);
+        }
+        Console.WriteLine(maxCustomers);
+    }
+}
